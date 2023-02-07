1f1464e189b78cf4d42daa32831018d348a658e9
Madi <Madison-CF@hotmail.fr>
2023-02-07 10:35:50 +0100

    links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,38 @@
-import styles from './../styles/Banner.module.scss';
+import styles from "./../styles/Banner.module.scss";
 import Image from "next/image";
-import logo from "../images/logo.png"
-
+import logo from "../images/logo.png";
+import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-   
-    <div className={styles.baneer}>
-      <Image src={logo} alt="logo" className={styles.banneer__logo} height={70} width={210}/>
-      <div className={styles.baneer__nav}>
-        <ul className={styles.baneer__nav__list}>
-          <li className={styles.baneer__nav__list__element}>Accueil</li>
-          <li className={styles.baneer__nav__list__element}>A propos</li>
-        </ul>
-      </div>
-    </div>
+  const [isActive, setIsActive] = useState("accueil");
 
+  const handleClick = (e, link) => {
+    setIsActive(link);
+    console.log(link);
+  };
+  return (
+    <header>
+      <div className={styles.baneer}>
+        <Image
+          src={logo}
+          alt="logo"
+          className={styles.banneer__logo}
+          height={70}
+          width={210}
+        />
+        <nav className={styles.baneer__nav}>
+          <ul className={styles.baneer__nav__list}>
+            <Link href="#" className={styles.navLink}>
+              <li className={styles.baneer__nav__list__elt}>Accueil</li>
+            </Link>
+            <Link href="#" className={styles.navLink}>
+              <li className={styles.baneer__nav__list__elt}>A propos</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
